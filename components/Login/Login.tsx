193c6e1e4f28f436ce2c0863d0373d04c881fddc
tohsaka888 <156132264@qq.com>
2021-11-09 14:31:49 +0800

    '我的'页面配置

--- components/Login/Login.tsx
+++ components/Login/Login.tsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {View, Text} from 'react-native';
+// import {View, Text} from 'react-native';
 import LoginTitle from './LoginTitle';
 import LoginContent from './LoginContent';
 import LoginModal from './LoginModal';
