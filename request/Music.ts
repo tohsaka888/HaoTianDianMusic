7d0716de5bc9b40c4f10e9e9c6b95d30eaded41f
tohsaka888 <156132264@qq.com>
2021-11-10 12:19:19 +0800

    优化上一首下一首请求传参

--- request/Music.ts
+++ request/Music.ts
@@ -1,18 +1,26 @@
 import {pythonUrl} from './baseUrl';
 
-const getNextMusic = async (playlistId: number, musicId: number) => {
-  const res = await fetch(`${pythonUrl}/next`, {
+const getNextMusic = async (
+  playlistId: number | string = '',
+  musicId: number,
+) => {
+  const res = await fetch(`${pythonUrl}/getNextMusic/next`, {
     method: 'POST',
     body: JSON.stringify({playlistId: playlistId, musicId: musicId}),
+    headers: {Accept: 'application/json', 'Content-Type': 'application/json'},
   });
   const data = await res.json();
   return data;
 };
 
-const getPreviousMusic = async (playlistId: number, musicId: number) => {
-  const res = await fetch(`${pythonUrl}/previous`, {
+const getPreviousMusic = async (
+  playlistId: number | string = '',
+  musicId: number,
+) => {
+  const res = await fetch(`${pythonUrl}/getNextMusic/previous`, {
     method: 'POST',
     body: JSON.stringify({playlistId: playlistId, musicId: musicId}),
+    headers: {Accept: 'application/json', 'Content-Type': 'application/json'},
   });
   const data = await res.json();
   return data;
