d2be1741cf3f825111205c4c80eabdf82438987b
tohsaka888 <156132264@qq.com>
2021-11-12 23:30:19 +0800

    link react-native-storage

--- android/app/src/main/java/com/awesometsproject/MainApplication.java
+++ android/app/src/main/java/com/awesometsproject/MainApplication.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
+import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import com.brentvatne.react.ReactVideoPackage;
 import com.brentvatne.react.ReactVideoPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -28,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
         // protected List<ReactPackage> getPackages() {
         //     return Arrays.asList(
         //             new MainReactPackage(),
+            new AsyncStoragePackage(),
             new AsyncStoragePackage(),
         //     new ReactVideoPackage(),
         //             new ReactVideoPackage()
