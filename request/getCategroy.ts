2e4bc9d4b91b5011ab35e1de7a05bdaf8675a9fc
tohsaka888 <156132264@qq.com>
2021-11-12 22:27:39 +0800

    优化分类请求

--- request/getCategroy.ts
+++ request/getCategroy.ts
@@ -3,7 +3,6 @@ import {baseUrl} from './baseUrl';
 const getCategroy = async () => {
   const res = await fetch(`${baseUrl}/playlist/catlist`);
   const data = await res.json();
-  // console.log(data.sub)
   return data.sub;
 };
 
