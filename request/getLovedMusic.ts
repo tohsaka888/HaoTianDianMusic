98e163805d6c314a705d145a33e2d1650cd0d08d
tohsaka888 <156132264@qq.com>
2021-11-17 19:18:09 +0800

    优化用户歌单请求

--- request/getLovedMusic.ts
+++ request/getLovedMusic.ts
@@ -5,14 +5,19 @@ const getLovedMusic = async () => {
   let loginStatus: any = {};
   try {
     loginStatus = await storage.load({key: 'loginStatus'});
-    const res = await fetch(`${pythonUrl}/getUL`, {
-      method: 'POST',
-      headers: {Accept: 'application/json', 'Content-Type': 'application/json'},
-      body: JSON.stringify({userId: loginStatus.userId}),
-    });
-    const data = await res.json();
-    if (data.success) {
-      return data.result[0];
+    if (loginStatus.userId) {
+      const res = await fetch(`${pythonUrl}/getUL`, {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({userId: loginStatus.userId}),
+      });
+      const data = await res.json();
+      if (data.success) {
+        return data.result[0];
+      }
     }
   } catch (error) {
     console.log(error);
