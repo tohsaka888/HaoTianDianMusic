23c0c0c50a7cc7b8b2fc4729a20dcba70773fdec
tohsaka888 <156132264@qq.com>
2021-11-25 09:49:23 +0800

    修改分类详情响应

---
+++ request/getCategoryDetail.ts
@@ -0,0 +1,13 @@
+import {pythonUrl} from './baseUrl';
+
+const getCategoryDetail = async (tags: string, page = 1) => {
+  const res = await fetch(`${pythonUrl}/getPLCat`, {
+    method: 'POST',
+    headers: {Accept: 'application/json', 'Content-Type': 'application/json'},
+    body: JSON.stringify({tags: tags, page: page}),
+  });
+  const data = await res.json();
+  return data.data || [];
+};
+
+export {getCategoryDetail};
