5f63352c80a67e251d7715be8d3af563688e0310
tohsaka888 <156132264@qq.com>
2021-11-27 20:07:13 +0800

    根据路由获取不同webview

--- components/echarts/EchartsTab.tsx
+++ components/echarts/EchartsTab.tsx
@@ -1,53 +1,72 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {StyleSheet, useWindowDimensions} from 'react-native';
 import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
 import {WebView} from 'react-native-webview';
+import {getAnalysisType} from '../../request/getAnalysisType';
 
 const Tab = createMaterialTopTabNavigator();
 
-const EchartsWebview = () => {
+const EchartsWebview = ({item}: {item: any}) => {
   return (
     <WebView
       style={styles.webview}
       nestedScrollEnabled={false}
       originWhitelist={['http://*', 'https://*']}
-      source={{uri: 'http://81.68.113.218:10086/'}}
+      source={{uri: `http://81.68.113.218:10086/${item?.url}`}}
     />
   );
 };
 
 export default function EchartsTab() {
+  const [analysisType, setAnalysisType] = useState<{name: string}[]>([]);
+  const width = useWindowDimensions().width;
+  useEffect(() => {
+    const getType = async () => {
+      const data = await getAnalysisType();
+      setAnalysisType(data);
+    };
+    getType();
+  }, []);
   return (
-    <Tab.Navigator>
-      <Tab.Screen
-        name="tags"
-        children={EchartsWebview}
-        options={{
-          tabBarScrollEnabled: true,
-          tabBarStyle: {
-            backgroundColor: 'transparent',
-          },
-          tabBarItemStyle: {
-            width: 100,
-            height: 50,
-          },
-          tabBarLabelStyle: {
-            color: 'white',
-            fontWeight: 'bold',
-          },
-          tabBarIndicatorStyle: {
-            backgroundColor: 'red',
-          },
-        }}
-      />
-    </Tab.Navigator>
+    <>
+      {analysisType.length !== 0 && (
+        <Tab.Navigator>
+          {analysisType.map((item, index) => {
+            return (
+              <Tab.Screen
+                name={item.name}
+                children={() => <EchartsWebview item={item} />}
+                key={index}
+                options={{
+                  tabBarScrollEnabled: true,
+                  tabBarStyle: {
+                    backgroundColor: 'transparent',
+                  },
+                  tabBarItemStyle: {
+                    width: width / 2,
+                    height: 50,
+                  },
+                  tabBarLabelStyle: {
+                    color: 'white',
+                    fontWeight: 'bold',
+                  },
+                  tabBarIndicatorStyle: {
+                    backgroundColor: 'red',
+                  },
+                }}
+              />
+            );
+          })}
+        </Tab.Navigator>
+      )}
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   webview: {
     flex: 1,
-    backgroundColor: '#ffffff14',
+    backgroundColor: 'transparent',
     borderRadius: 10,
   },
 });
